e95e7d25dbf7e710d3536619b70bd5e08e10045c
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-26 04:49:44 +0600

    ✏️ Fixed Typos

--- src/Modules/HomePage/Donation/Donation.jsx
+++ src/Modules/HomePage/Donation/Donation.jsx
@@ -9,12 +9,12 @@ const Donation = () => {
   return (
     <section className="bg-primary rounded-lg  bg-opacity-15 flex flex-col justify-center items-center pb-6 ">
       <h1 className="text-4xl font-museo text-center pt-12 pb-6">Donate Now</h1>
-      <p className="text-center w-2/4  mx-auto pb-6">
+      {/* <p className="text-center w-2/4  mx-auto pb-6">
         Donating during floods can provide essential resources such as food,
         shelter, and medical aid to those affected by the disaster. Your
         contribution helps save lives, rebuild communities, and offer hope in
         times of crisis.
-      </p>
+      </p> */}
       <div className="grid place-content-center grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 w-full mx-auto min-h-screen items-center justify-center pb-6">
         {DonationSites.map((item, index) => (
           <div key={index} className="p-2 ">
@@ -26,7 +26,7 @@ const Donation = () => {
           </div>
         ))}
       </div>
-      <Button
+      {/* <Button
         variant="outline"
         className="p-6 bg-primary text-secondary hover:text-primary  hover:bg-secondary text-xl"
       >
@@ -36,7 +36,7 @@ const Donation = () => {
         >
           Show More <ArrowRightIcon className="ms-2" />
         </a>
-      </Button>
+      </Button> */}
     </section>
   );
 };
