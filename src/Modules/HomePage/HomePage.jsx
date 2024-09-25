295a8f3186663d2ba526bb70a198d000c7542cde
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-26 04:49:14 +0600

    ⚡️  Added newly created Components to Home Page

--- src/Modules/HomePage/HomePage.jsx
+++ src/Modules/HomePage/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Donation from "./Donation/Donation";
 import Stats from "./Stats/Stats";
+import LatestNewsSection from "./News/LatestNewsSection";
+import PrecautionSection from "./Precaution/PrecautionSection";
+import WeatherDashboard from "./Weather/WeatherDashboard";
+import WeatherForecastComponent from "./Weather/WeatherForecastComponent";
 
 const HomePage = () => {
   return (
     <section className="w-10/12 mx-auto ">
+      <WeatherDashboard />
+      <WeatherForecastComponent />
       <Stats />
       <Donation />
+      <LatestNewsSection />
+      <PrecautionSection />
     </section>
   );
 };
