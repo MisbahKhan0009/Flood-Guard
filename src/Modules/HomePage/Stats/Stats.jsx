6839392049514795d319dfbee845178c36b5a191
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-26 03:13:04 +0600

    ✨ Added Project impact part

---
+++ src/Modules/HomePage/Stats/Stats.jsx
@@ -0,0 +1,25 @@
+import { statsData } from "./statsData";
+
+const StatsCards = () => {
+  return (
+    <section className="bg-primary rounded-lg bg-opacity-15 flex flex-col justify-center items-center my-6">
+      <h1 className="text-4xl font-museo text-center pt-12">Our Impact</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-10/12 mx-auto text-center py-12">
+        {statsData.map((stat, index) => (
+          <div
+            key={index}
+            className={`p-6 bg-gradient-to-r ${stat.gradient} rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300`}
+          >
+            <stat.icon className={`${stat.color} text-5xl mx-auto mb-4`} />{" "}
+            <p className={`text-5xl font-extrabold ${stat.color}`}>
+              {stat.value}
+            </p>
+            <p className="text-md text-gray-500 mt-2">{stat.description}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default StatsCards;
---
+++ src/Modules/HomePage/Stats/statsData.js
@@ -0,0 +1,25 @@
+import { FaPeopleCarry, FaMoneyBillWave, FaUsers } from "react-icons/fa";
+
+export const statsData = [
+  {
+    icon: FaPeopleCarry,
+    value: "33.3k+",
+    color: "text-blue-600",
+    description: "People Rescued Till Now",
+    gradient: "from-blue-50 to-blue-100",
+  },
+  {
+    icon: FaMoneyBillWave,
+    value: "50Cr.",
+    color: "text-green-600",
+    description: "Taka Donation Received",
+    gradient: "from-green-50 to-green-100",
+  },
+  {
+    icon: FaUsers,
+    value: "50k+",
+    color: "text-purple-600",
+    description: "Total Users",
+    gradient: "from-purple-50 to-purple-100",
+  },
+];
