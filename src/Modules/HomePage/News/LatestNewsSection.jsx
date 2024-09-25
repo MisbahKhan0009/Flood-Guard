b10ca113af28be8f76b5a83d70a0c912e2b45558
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-26 04:48:02 +0600

    ✨  News Article Section added

---
+++ src/Modules/HomePage/News/LatestNewsSection.jsx
@@ -0,0 +1,44 @@
+// src/components/LatestNewsSection.jsx
+import React from "react";
+import { newsData } from "./newsData";
+
+const LatestNewsSection = () => {
+  return (
+    <section className="bg-primary bg-opacity-15 py-12 my-6 rounded-lg">
+      <h1 className="text-4xl font-museo text-center mb-8 text-primary">
+        Latest News
+      </h1>
+      <div className="w-9/12 mx-auto space-y-8">
+        {newsData.map((news) => (
+          <div
+            key={news.id}
+            className="flex flex-col md:flex-row items-center bg-primary shadow-lg rounded-lg overflow-hidden transition hover:shadow-xl"
+          >
+            <div className="md:w-1/3">
+              <img
+                src={news.imageUrl}
+                alt={news.title}
+                className="w-full h-full object-fill md:h-full"
+              />
+            </div>
+
+            <div className="md:w-2/3 p-6">
+              <h2 className="text-2xl font-bold mb-4 text-secondary">
+                {news.title}
+              </h2>
+              <p className="text-gray-600 ">{news.description}</p>
+              <a
+                href={news.link}
+                className="text-secondary underline text-sm"
+              >
+                Read More
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default LatestNewsSection;
---
+++ src/Modules/HomePage/News/newsData.js
@@ -0,0 +1,30 @@
+// src/data/newsData.js
+export const newsData = [
+  {
+    id: 1,
+    imageUrl:
+      "https://www.aljazeera.com/wp-content/uploads/2024/08/AFP__20240822__36EN4HU__v3__HighRes__TopshotBangladeshWeatherFlood-1724392163.jpg?resize=1170%2C780&quality=100",
+    title: "Deadly floods leave millions stranded in Bangladesh",
+    description:
+      "More than four million people have been affected by the floods triggered by torrential rains, with hundreds of homes under water...",
+    link: "https://www.aljazeera.com/gallery/2024/8/23/deadly-floods-leave-millions-stranded-in-bangladesh",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://www.aljazeera.com/wp-content/uploads/2024/08/AFP__20240823__36EN3LM__v1__HighRes__BangladeshWeatherFlood-1724392266.jpg?resize=1170%2C780&quality=100",
+    title: "When floods came, students rose to challenge",
+    description:
+      "Hundreds of students from Dhaka, Chattogram and districts in between were seen marching towards Feni on August 22. They travelled in trucks, with speedboats...",
+    link: "https://www.thedailystar.net/weekend-read/news/when-floods-came-students-rose-challenge-3690711",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://www.aljazeera.com/wp-content/uploads/2024/08/AFP__20240822__36EK2HP__v4__HighRes__BangladeshWeatherFlood-1724392143.jpg?fit=1170%2C780&quality=80",
+    title: "Floods kill more than 30, impact millions in Bangladesh",
+    description:
+      "Rescuers are scrambling to evacuate flooded communities after heavy rains inundated parts of Bangladesh and northeast India, killing at least 36...",
+    link: "https://edition.cnn.com/2024/08/22/india/flooding-bangladesh-india-intl-hnk/index.html",
+  },
+];
