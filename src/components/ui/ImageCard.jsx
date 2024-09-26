01604f329e5a2d24997fa48f4f6d889fffb0f686
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-27 01:37:21 +0600

    💄 Fixed Style issue

--- src/components/ui/ImageCard.jsx
+++ src/components/ui/ImageCard.jsx
@@ -1,19 +1,23 @@
 // ImageCard.jsx
 import React from "react";
-import { Button } from "@/components/ui/button"
-
+import { Button } from "@/components/ui/button";
 
 const ImageCard = ({ imgUrl, title, donationLink }) => {
   return (
-    <div className="border mx-auto bg-primary text-secondary w-9/12 rounded-lg shadow-lg overflow-hidden">
+    <div className="border mx-auto bg-primary text-secondary rounded-lg shadow-lg overflow-hidden">
       <a href={donationLink} target="_blank" rel="noopener noreferrer">
-        <img src={imgUrl} alt={title} className="w-auto mx-auto pt-4 h-48 object-fit" />
+        <img
+          src={imgUrl}
+          alt={title}
+          className="w-auto mx-auto pt-4 h-48 object-fit"
+        />
         <div className="p-6">
-          <h3 className="text-lg font-semibold">{title}</h3>
-          <Button variant="outline" className="mt-6">Donate now</Button>
+          <h3 className="text-lg font-semibold truncate">{title}</h3>
+          <Button variant="outline" className="mt-6">
+            Donate now
+          </Button>
           <div className="pt-2  text-secondary"></div>
         </div>
-        
       </a>
     </div>
   );
