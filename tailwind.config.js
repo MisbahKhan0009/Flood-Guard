b811b8b1bbbafb0128116886aa117df181e18606
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-27 04:58:56 +0600

    💫 Added Accordion From Shad/cn

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
-      center: true,
+      center: "true",
       padding: "2rem",
       screens: {
         "2xl": "1400px",
@@ -20,11 +20,11 @@ export default {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "#cecedf", // Updated primary color
+          DEFAULT: "#cecedf",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: "#051224", // Updated secondary color
+          DEFAULT: "#051224",
           foreground: "hsl(var(--secondary-foreground))",
         },
       },
@@ -40,17 +40,43 @@ export default {
       },
       keyframes: {
         "slide-in": {
-          "0%": { width: "0%" },
-          "100%": { width: "100%" },
+          "0%": {
+            width: "0%",
+          },
+          "100%": {
+            width: "100%",
+          },
         },
         "slide-out": {
-          "0%": { width: "100%" },
-          "100%": { width: "0%" },
+          "0%": {
+            width: "100%",
+          },
+          "100%": {
+            width: "0%",
+          },
+        },
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
         },
       },
       animation: {
         "slide-in": "slide-in 0.4s forwards",
         "slide-out": "slide-out 0.4s forwards",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
