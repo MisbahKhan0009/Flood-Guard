353e34aadd6f4c477895b94c4d675fbffe4cecfa
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-27 01:41:06 +0600

    🐛 Fixed Homepage Layout

--- src/Modules/HomePage/HomePage.jsx
+++ src/Modules/HomePage/HomePage.jsx
@@ -4,13 +4,11 @@ import Stats from "./Stats/Stats";
 import LatestNewsSection from "./News/LatestNewsSection";
 import PrecautionSection from "./Precaution/PrecautionSection";
 import WeatherDashboard from "./Weather/WeatherDashboard";
-import WeatherForecastComponent from "./Weather/WeatherForecastComponent";
 
 const HomePage = () => {
   return (
     <section className="w-10/12 mx-auto ">
       <WeatherDashboard />
-      <WeatherForecastComponent />
       <Stats />
       <Donation />
       <LatestNewsSection />
