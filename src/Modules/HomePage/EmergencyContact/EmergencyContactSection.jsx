3b837c2b743ecb757fffa71a88549eb2ac8fbbce
MisbahKhan0009 <misbah.khan@northsouth.com>
2024-09-27 06:12:28 +0600

    ✨ Added Emergency COntact Section

--- src/Modules/HomePage/EmergencyContact/EmergencyContactSection.jsx
+++ src/Modules/HomePage/EmergencyContact/EmergencyContactSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { emergencyContacts } from "./EmergencyInfo";
+import { Phone } from "lucide-react";
 
 const EmergencyContactSection = () => {
   return (
@@ -7,14 +8,17 @@ const EmergencyContactSection = () => {
       <h2 className="text-4xl  text-primary text-center font-museo my-6 mb-12">
         Emergency Contact Information
       </h2>
-      <div className="grid grid-cols-1 text-center w-10/12 mx-auto sm:grid-cols-1 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 text-center w-11/12 mx-auto sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {emergencyContacts.map((contact, index) => (
           <div
             key={index}
             className="text-secondary p-4 rounded-lg shadow-md bg-primary"
           >
-            <h3 className="font-thin mb-6 text-2xl">{contact.name}</h3>
-            <p className="mt-2 font-light">{contact.number}</p>
+            <contact.icon className="text-6xl mx-auto my-6 text-secondary" />
+            <h3 className="font-thin mb-6 text-4xl">{contact.name}</h3>
+            <p className="my-2 font-light flex items-center justify-center">
+              <Phone className="me-2 font-light" /> {contact.number}
+            </p>
           </div>
         ))}
       </div>
--- src/Modules/HomePage/EmergencyContact/EmergencyInfo.js
+++ src/Modules/HomePage/EmergencyContact/EmergencyInfo.js
@@ -1,26 +1,38 @@
+import { FaPersonMilitaryRifle } from "react-icons/fa6";
+import { LuHelpingHand } from "react-icons/lu";
+import { MdOutlineFireTruck } from "react-icons/md";
+import { TbSailboat } from "react-icons/tb";
+import { IoHelpBuoySharp } from "react-icons/io5";
+import { PiAmbulance } from "react-icons/pi";
 export const emergencyContacts = [
   {
     name: "National Helpline",
     number: "999",
+    icon: LuHelpingHand,
   },
   {
-    name: "Fire Service and Civil Defence",
+    name: "Fire Service",
     number: "+02-2233555555",
+    icon: MdOutlineFireTruck,
   },
   {
     name: "Army Helpline",
-    number: "01550150061",
+    number: "+88015501-50061",
+    icon: FaPersonMilitaryRifle,
   },
   {
     name: "Coast Guard Helpline",
     number: "+8804662-75341",
+    icon: IoHelpBuoySharp,
   },
   {
-    name: "Helpline 1",
+    name: "Emergency Ambulance",
     number: "+8801234-56789",
+    icon: PiAmbulance,
   },
   {
-    name: "Helpline 2",
+    name: "Emergency Boat Service" ,
     number: "+8809876-54321",
+    icon: TbSailboat,
   },
 ];
